4fe534f013c80831223f8f80f065954719aa0fb2
Jesús A. Nieves <73083779+nievesj04@users.noreply.github.com>
2024-03-16 10:49:30 +0800

    Add files via upload

--- Login_CSharp/Program.cs
+++ Login_CSharp/Program.cs
@@ -11,54 +11,94 @@ namespace Login
             InitializeComponent();
         }
 
-        // Conección de C# con la base de datos de SQL Server  | Nombre del servidor| nombre de la BD | Seguridad |
-        SqlConnection con = new SqlConnection(@"Data Source=(LocalDb)\v11.0;Initial Catalog=Login_csharp;Integrated Security=True");
-
+        
         // Metodo para validar los datos del Login
-        public void Logear(string user, string password)
+        public bool Logear(string codUsuaIngresado, string contraIngresada)
         {
-            // Manejo de errores
-            try
-            {
-                //Abrimos la conexion
-                con.Open(); 
-                // Consulta SQL para la peticion de los datos
-                SqlCommand cmd = new SqlCommand("SELECT Name, User_type FROM Ususarios WHERE User = @user AND Password = @pass", con);
-                // Agregando los parametros al comando SQL
-                cmd.Parameters.AddWithValue("user", user);
-                cmd.Parameters.AddWithValue("pass", password);
-                // Objeto DataAdapter
-                SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-                sda.Fill(dt);
+            string serverName = ""; // Nombre del servidor
+            string dbName = ""; // Nombre de la Base de Datos
+            string userId = ""; // Id del usuario DB
+            string passwordConnection = ""; // Contraseña de conexión
 
-                // Verifica y cuenta que existan filas en la base de datos
-                if(dt.Rows.Count == 1)
+            string decryptPassword = ""; // Contraseña para desencriptar la contraseña de la base de datos
+            
+            using (SqlConnection connection = new SqlConnection($"Data Source={serverName}; Initial Catalog={dbName}; Persist Security Info=True; User ID={userId}; Password={passwordConnection}"))
+            {
+                string query = $"SELECT DECRYPTBYPASSPHRASE('{decryptPassword}', Contra) AS Contra,\n" + 
+                                "EsAdmin\n"+
+                                "FROM Usuarios\n"+
+                                "WHERE CodUsua = @codUsua\n";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
                 {
-                    this.Hide();
-                    if (dt.Rows[0][1].ToString() == "Admin");  //Verifica si el usuasio que se esta autenticando es "Usuario o Admin"
-                    {     
-                        new Admin(dt.Rows[0][0].ToString()).Show(); //Muestra la ventana admin en caso de ser "Admin"
-                    } 
-                    else if (dt.Rows[0][1].ToString () == "Usuario") //Verifica si el usuasio que se esta autenticando es "Usuario o Admin"
+                    cmd.Parameters.AddWithValue("@codUsua", codUsuaIngresado);
+
+                    try
                     {
-                        new Usuario (dt.Rows[0][0].ToString()).Show(); //Muestra la ventana usuario en caso de ser "Usuario"
-                    }          
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (cmd.ExecuteNonQuery() == 1) 
+                            {
+                                if (reader.Read())
+                                {
+                                    string contraBaseDatos = reader["Contra"].ToString();
+                                    string esAdmin = reader["EsAdmin"].ToString();
+
+                                    if (contraIngresada == contraBaseDatos) { return true; } 
+                                    else { return false; }
+                                }
+                                else { return false; }
+                            }
+                            else { return false; }
+                        }
+                    }   
                 }
-                else
-                {
-                    MessageBox.Show("Usuario o Contraseña Incorrecta");
-                }
-            }
-            catch(Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-            finally
-            {
-                // Cierra la conección a la Base de Datos
-                con.Close();
-            }
+            } 
         }
     }
 }
+
+
+
+                    // // Manejo de errores
+                    // try
+                    // {
+                    //     //Abrimos la conexion
+                    //     con.Open(); 
+                    //     // Consulta SQL para la peticion de los datos
+                    //     SqlCommand cmd = new SqlCommand("SELECT Name, User_type FROM Ususarios WHERE CodUsua = @CodUsua AND Contra = @Contra", con);
+                    //     // Agregando los parametros al comando SQL
+                    //     cmd.Parameters.AddWithValue("@CodUsua", codUsuaIngresado);
+                    //     cmd.Parameters.AddWithValue("@Contra", contraIngresada);
+                    //     // Objeto DataAdapter
+                    //     SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    //     DataTable dt = new DataTable();
+                    //     sda.Fill(dt);
+
+                    //     // Verifica y cuenta que existan filas en la base de datos
+                    //     if(dt.Rows.Count == 1)
+                    //     {
+                    //         this.Hide();
+                    //         if (dt.Rows[0][1].ToString() == "Admin");  //Verifica si el usuasio que se esta autenticando es "Usuario o Admin"
+                    //         {     
+                    //             new Admin(dt.Rows[0][0].ToString()).Show(); //Muestra la ventana admin en caso de ser "Admin"
+                    //         } 
+                    //         else if (dt.Rows[0][1].ToString () == "Usuario") //Verifica si el usuasio que se esta autenticando es "Usuario o Admin"
+                    //         {
+                    //             new Usuario (dt.Rows[0][0].ToString()).Show(); //Muestra la ventana usuario en caso de ser "Usuario"
+                    //         }          
+                    //     }
+                    //     else
+                    //     {
+                    //         MessageBox.Show("Usuario o Contraseña Incorrecta");
+                    //     }
+                    // }
+                    // catch(Exception e)
+                    // {
+                    //     MessageBox.Show(e.Message);
+                    // }
+                    // finally
+                    // {
+                    //     // Cierra la conección a la Base de Datos
+                    //     con.Close();
+                    // }
--- Login_CSharp/SQLQuery1.sql
+++ Login_CSharp/SQLQuery1.sql
@@ -2,15 +2,15 @@ use Login_csharp
 
 CREATE TABLE [dbo].[Usuarios]
 (
-    [ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    [Name] VARCHAR(50) NULL,
-    [User] VARCHAR(50) NULL,
-    [Password] VARCHAR(50) NULL,
-    [User_type] VARCHAR(50) NULL,
+    [ID] [int] IDENTITY(1,1) NOT NULL,
+    [Name] [varchar](50) NULL,
+    [CodUsua] [varchar](20) NOT NULL,
+    [Contra] [varbinary](max) NULL,
+    [EsAdmin] [smallint] NULL,
 )
 
 SELECT * FROM Usuarios
 
 -- Insertando Valores
-INSERT INTO Usuarios VALUES ('Jesus', 'Jesus004', '12345', 'Admin'),
-                            ('Ricardo', 'Ricardo007', '54321', 'User');
+INSERT INTO Usuarios VALUES ('Jesus', 'Jesus004', '12345', 1),
+                            ('Ricardo', 'Ricardo007', '54321', 0);
